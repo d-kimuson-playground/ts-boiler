8a176b9c0d2dd4ada9eeb34ec11f3f008bdcbc53
d-kimsuon <d-kimuson@gmail.com>
2022-05-16 02:32:00 +0900

    chore(eslint): update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,6 +23,7 @@ module.exports = {
     jest: true,
   },
   rules: {
+    // typescript
     "@typescript-eslint/explicit-function-return-type": "error",
     "@typescript-eslint/explicit-module-boundary-types": "error",
     "@typescript-eslint/no-unused-vars": [
@@ -33,20 +34,34 @@ module.exports = {
         args: "after-used",
       },
     ],
-    "@typescript-eslint/ban-ts-comment": "warn",
+    "@typescript-eslint/ban-ts-comment": [
+      "error",
+      {
+        "ts-expect-error": "allow-with-description",
+        "ts-ignore": true,
+        "ts-nocheck": "allow-with-description",
+        "ts-check": false,
+        minimumDescriptionLength: 1,
+      },
+    ],
     "@typescript-eslint/explicit-member-accessibility": "warn",
-    "no-console": "warn",
     "@typescript-eslint/prefer-ts-expect-error": "error",
     "@typescript-eslint/consistent-type-imports": "error",
     "@typescript-eslint/no-non-null-assertion": "error",
     "@typescript-eslint/prefer-nullish-coalescing": "error",
-    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-explicit-any": [
+      "error",
+      {
+        fixToUnknown: true,
+      },
+    ],
     "@typescript-eslint/consistent-type-definitions": ["error", "type"],
     "@typescript-eslint/method-signature-style": ["error", "property"],
     "@typescript-eslint/prefer-for-of": "error",
     "@typescript-eslint/no-unnecessary-condition": "error",
     "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
-    "@typescript-eslint/explicit-function-return-type": "warn",
+    // common
+    "no-console": "warn",
     "import/order": [
       "error",
       {
